15dd44e0517665df832bde4d4bf022b825b14259
feliperdamaceno <feliperdamaceno@sent.com>
2024-01-28 00:27:41 +0000

    feat: Implement color picker on grid color tiles.

--- client/src/components/RGBTile.vue
+++ client/src/components/RGBTile.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
 import type { RGBColor } from '../types'
+import colorConverter from 'color-convert'
 
-defineProps<{
+const { color } = defineProps<{
   color: RGBColor
 }>()
 </script>
 
 <template>
-  <div
-    :style="{ backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})` }"
-    class="w-20 border-2 rounded-sm aspect-square border-zinc-200"
-  />
+  <div class="w-20 h-20 border-2 border-zinc-200">
+    <input
+      class="w-full h-full"
+      type="color"
+      :value="`#${colorConverter.rgb.hex(color.r, color.g, color.b)}`"
+    />
+  </div>
 </template>
+
+<style lang="postcss">
+input[type='color'],
+input[type='color']::-webkit-color-swatch,
+input[type='color']::-webkit-color-swatch-wrapper {
+  -webkit-appearance: none;
+  @apply border-none p-0 cursor-pointer;
+}
+</style>
